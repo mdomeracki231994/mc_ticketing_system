b8ee35275798142b6bce7263844ad1a84cd98dbb
mdomeracki231994 <domerackimark23@gmail.com>
2024-11-14 16:21:42 -0500

    Switching up database

--- mc_ticketing_system/settings.py
+++ mc_ticketing_system/settings.py
@@ -84,17 +84,19 @@ WSGI_APPLICATION = 'mc_ticketing_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 if "DATABASE_URL" in os.environ:
     import dj_database_url
 
     DATABASES = {"default": dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
